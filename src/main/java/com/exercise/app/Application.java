57cb358679e053ebd391363a655c1fdff1716bff
arechkalov <antony.rechkalov@gmail.com>
2017-02-26 12:48:37 +0200

    changes in configuration

--- src/main/java/com/exercise/app/Application.java
+++ src/main/java/com/exercise/app/Application.java
@@ -1,23 +1,17 @@
 package com.exercise.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 /**
  * Created by arecicalov on 2/20/2017.
  */
-@EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages = "com.exercise*")
 public class Application {
     public static void main(String[] args) throws IOException {
-        ConfigurableApplicationContext ctx
-                = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
